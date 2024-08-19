6fbf680a4f40b15710a18cafc6767241744bc64f
luxluth <delphin.blehoussi93@gmail.com>
2024-08-19 21:13:21 +0200

    Update `hello_world` example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,7 +1,10 @@
+use mini_server::worker::ThreadPool;
 use mini_server::*;
-
 fn main() {
-    let mut app = HTTPServer::default();
+    let mut app = HTTPServer {
+        thread_pool: ThreadPool::new(4),
+        ..Default::default()
+    };
 
     app.get("/", |_, _| {
         let mut response = HTTPResponse::default();
